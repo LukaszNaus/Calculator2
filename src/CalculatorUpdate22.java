7a027239bc809fa691c991a86170e1d122a24b79
LukaszNaus <nauslukasz@gmail.com>
2022-09-21 20:37:12 +0200

    Added addition method

---
+++ src/CalculatorUpdate22.java
@@ -0,0 +1,6 @@
+public class CalculatorUpdate22 {
+    public int addition(int x, int y){
+        int z = x+y;
+        return z;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CalculatorUpdate22 kalkulator = new CalculatorUpdate22();
+        System.out.println(kalkulator.addition(4,6));
+    }
+}

5f0e8df270f92bb4b79a05a5f721a3ffc0523de4
LukaszNaus <nauslukasz@gmail.com>
2022-09-21 20:48:47 +0200

    Added addition method,substraction multiply and delay

--- src/CalculatorUpdate22.java
+++ src/CalculatorUpdate22.java
@@ -3,4 +3,14 @@ public class CalculatorUpdate22 {
         int z = x+y;
         return z;
     }
+    public int substraction(int x, int y){
+        return x-y;
+    }
+    public int multiply(int x, int y) {
+        return x*y;
+    }
+    public int delay(int x, int y) {
+        return x / y;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         CalculatorUpdate22 kalkulator = new CalculatorUpdate22();
         System.out.println(kalkulator.addition(4,6));
+        System.out.println(kalkulator.substraction(10,5));
+        System.out.println(kalkulator.multiply(3,5));
+        System.out.println(kalkulator.delay(40,10));
     }
 }
